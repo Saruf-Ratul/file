a4064856d80005e2acbc158646b77141cc8dfd08
Saruf-Ratul <saruftoratul95@gmail.com>
2022-05-08 23:55:00 +0600

    first commit

---
+++ File-Web-Application/resources/views/filemanager.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>File Manager Web Application</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css">
+    <link href="{{ asset('vendor/file-manager/css/file-manager.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    <div class="jumbotron">
+        <h1>File Manager Web Application</h1>
+    </div>
+        <div class="row">
+            <div class="col-md-12" id="fm-main-block">
+                <div id="fm"></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- File manager -->
+    <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('fm-main-block').setAttribute('style', 'height:' + window.innerHeight + 'px');
+
+        fm.$store.commit('fm/setFileCallBack', function(fileUrl) {
+          window.opener.fmSetLink(fileUrl);
+          window.close();
+        });
+      });
+    </script>
+</body>
+</html>
